dd4e1291fffac25dd92df9d9871123c9480957a9
Laurent Arifaj <laurent.arifaj@studentet.uni-pr.edu>
2019-01-04 03:24:38 +0100

    1

--- javascript/booking.js
+++ javascript/booking.js
@@ -1,35 +1,35 @@
 /*eslint-env browser*/
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var number = 1;
+showImages(number);
 
-function plusSlides(n) {
-	showSlides(slideIndex += n);
+function plusphotos(n) {
+	showImages(number += n);
 }
 
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+	showImages(number = n);
 }
 
-function showSlides(n) {
+function showImages(n) {
 	var i;
-	var slides = document.getElementsByClassName("slides");
+	var photos = document.getElementsByClassName("photos");
 	var dots = document.getElementsByClassName("small-photo");
 	var captionText = document.getElementById("caption");
-	if (n > slides.length) {
-		slideIndex = 1;
+	if (n > photos.length) {
+		number = 1;
 	}
 	if (n < 1) {
-		slideIndex = slides.length;
+		number = photos.length;
 	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+	for (i = 0; i < photos.length; i++) {
+		photos[i].style.display = "none";
 	}
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
-	captionText.innerHTML = dots[slideIndex - 1].alt;
+	photos[number - 1].style.display = "block";
+	dots[number - 1].className += " active";
+	captionText.innerHTML = dots[number - 1].alt;
 }
